6ed0da61ff81722b56cd3912db7f0cdd3bf996ef
Lachlan <Lachlan@163.com>
2022-10-30 19:50:39 +0800

    pro1A第三次提交

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -4,100 +4,77 @@ public class ArrayDeque<T> {
     private int right;
     private int capacity = 8;
 
-    public ArrayDeque()
-    {
+    public ArrayDeque() {
         items = (T[]) new Object[capacity];
         left = right = 0;
     }
-    public int size()
-    {
+
+    public int size() {
         return (right - left + capacity) % capacity;
     }
 
-    public void resize(int newSize)
-    {
+    public void resize(int newSize) {
         T[] newArray = (T[]) new Object[newSize];
         int size = size();
 
-        if(left < right)
-        {
-            for(int i =left, j = 0; i < right && j < size; i++, j++)
-            {
+        if (left < right) {
+            for (int i = left, j = 0; i < right && j < size; i++, j++) {
                 newArray[j] = items[i];
             }
-        }
-        else if(left > right)
-        {
+        } else if (left > right) {
             int j = 0;
-            for(int i = left; j < capacity - left; i++, j++)
-            {
+            for (int i = left; j < capacity - left; i++, j++) {
                 newArray[j] = items[i];
             }
-            for(int i = 0; j < size; i++, j++)
-            {
+            for (int i = 0; j < size; i++, j++) {
                 newArray[j] = items[i];
             }
         }
         left = 0;
         right = size;
-        capacity =newSize;
+        capacity = newSize;
         items = newArray;
     }
 
-    public void addFirst(T item)
-    {
-        if(isFull())
-        {
-            resize((int)(capacity*1.5));
+    public void addFirst(T item) {
+        if (isFull()) {
+            resize((int) (capacity * 1.5));
         }
         left = (left - 1 + capacity) % capacity;
         items[left] = item;
     }
 
-    public void addLast(T item)
-    {
-        if(isFull())
-        {
-            resize((int)(capacity*1.5));
+    public void addLast(T item) {
+        if (isFull()) {
+            resize((int) (capacity * 1.5));
         }
         right = (right + 1 + capacity) % capacity;
         items[right] = item;
     }
 
-    public boolean isFull()
-    {
+    private boolean isFull() {
         return capacity == size();
     }
 
-    public boolean isEmpty()
-    {
+    private boolean isEmpty() {
         return size() == 0;
     }
 
-    public void printDeque()
-    {
-        if(left < right)
-        {
-            for(int i = left; i < right; i++)
-            {
-                if (i == right -1)
-                {
+    public void printDeque() {
+        if (left < right) {
+            for (int i = left; i < right; i++) {
+                if (i == right - 1) {
                     System.out.println(items[i]);
                     break;
                 }
                 System.out.print(items[i] + " ");
             }
-        }
-        else if(left > right)
-        {
-            for (int i = left; i < capacity; i++)
-            {
+        } else if (left > right) {
+            for (int i = left; i < capacity; i++) {
                 System.out.print(items[i] + " ");
             }
-            for (int i = 0; i <= right; i++)
-            {
-                if(i == right)
-                {
+            for (int i = 0; i <= right; i++) {
+                if (i == right) {
                     System.out.println(items[i]);
                     break;
                 }
@@ -107,13 +84,11 @@ public class ArrayDeque<T> {
 
     }
 
-    public boolean isLower()
-    {
-        return  capacity >= 16 && size() / (double) capacity < 0.25;
+    private boolean isLower() {
+        return capacity >= 16 && size() / (double) capacity < 0.25;
     }
 
-    public T removeFirst()
-    {
+    public T removeFirst() {
         if (isEmpty()) {
             return null;
         }
@@ -125,8 +100,7 @@ public class ArrayDeque<T> {
         return res;
     }
 
-    public T removeLast()
-    {
+    public T removeLast() {
         if (isEmpty()) {
             return null;
         }
